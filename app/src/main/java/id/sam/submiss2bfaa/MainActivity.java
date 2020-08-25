43965436c22573ae884af0ddebead3baf842236a
samga <samgalaxy111111@gmail.com>
2020-08-25 17:40:40 +0700

    update Action Bar, search dan language

--- app/src/main/java/id/sam/submiss2bfaa/MainActivity.java
+++ app/src/main/java/id/sam/submiss2bfaa/MainActivity.java
@@ -1,8 +1,13 @@
 package id.sam.submiss2bfaa;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.search:
+                Toast.makeText(MainActivity.this, "Tombol Search", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.language:
+                Toast.makeText(MainActivity.this, "Menu ini masih dalam pengembangan", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return true;
+        }
     }
 }
