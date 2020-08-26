a250ba9b19c5074dfcc77f039cdacd3f1d70676b
samga <samgalaxy111111@gmail.com>
2020-08-26 22:51:51 +0700

    Detail User

--- app/src/main/java/id/sam/submiss2bfaa/adapter/UserAdapter.java
+++ app/src/main/java/id/sam/submiss2bfaa/adapter/UserAdapter.java
@@ -1,9 +1,12 @@
 package id.sam.submiss2bfaa.adapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,6 +16,7 @@ import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import id.sam.submiss2bfaa.DetailActivity;
 import id.sam.submiss2bfaa.R;
 import id.sam.submiss2bfaa.model.UserItems;
 import jp.wasabeef.picasso.transformations.CropCircleTransformation;
@@ -28,14 +32,33 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
 
     @NonNull
     @Override
-    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int position) {
-        View mView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.user_item, viewGroup, false);
-        return new UserViewHolder(mView);
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        final View mView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.user_item, viewGroup, false);
+        final UserViewHolder userViewHolder = new UserViewHolder(mView);
+
+        userViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(userViewHolder.itemView.getContext(), mData.get(userViewHolder.getAdapterPosition()).getUsername(),Toast.LENGTH_SHORT).show();
+                Context context = mView.getContext();
+                int position = userViewHolder.getAdapterPosition();
+                Intent intent = new Intent(context, DetailActivity.class);
+                intent.putExtra("detail", mData.get(position).getUsername());
+                context.startActivity(intent);
+            }
+        });
+        return userViewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull UserViewHolder userViewHolder, int position) {
-        userViewHolder.bind(mData.get(position));
+    public void onBindViewHolder(@NonNull final UserViewHolder holder, int position) {
+        holder.bind(mData.get(position));
+//        holder.itemView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(holder.itemView.getContext(), mData.get(holder.getAdapterPosition()).getUsername(),Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 
     @Override
